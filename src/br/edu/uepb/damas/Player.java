69ddb1ebc153eee68ea1af7a508a7e52ae43cd1b
zemalay <geovanniovinhas@192.168.25.12>
2016-08-26 20:07:02 -0300

    classe player

---
+++ src/br/edu/uepb/damas/Player.java
@@ -0,0 +1,14 @@
+package br.edu.uepb.damas;
+
+/**
+ * @author geovanniovinhas
+ *
+ * Aug 26, 2016
+ */
+public class Player {
+
+	public Player() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
